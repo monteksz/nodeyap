e27471771ae43d9ac0b605a188229c2cf7dc53cf
Mizan <39762712+monteksz@users.noreply.github.com>
2024-11-14 12:05:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ MAX_CONNECTIONS = 15
 
 DOMAIN_API = {
     "SESSION": "https://api.nodepay.ai/api/auth/session",
-    "PING": "http://52.77.10.116/api/network/ping"
+    "PING": "http://13.215.134.222/api/network/ping"
 }
 
 CONNECTION_STATES = {
