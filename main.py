189758b354f714951b8ab0c491cd08172255e95d
Mizan <39762712+monteksz@users.noreply.github.com>
2024-11-29 10:02:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ DOMAIN_API = {
     "SESSION": "https://api.nodepay.ai/api/auth/session",
     "PING": [
         "http://13.215.134.222/api/network/ping",
-        "http://52.77.10.116/api/network/ping"
+        "http://18.139.20.49/api/network/ping",
+        "http://52.74.35.173/api/network/ping",
+        "http://52.77.10.116/api/network/ping",
+        "http://3.1.154.253/api/network/ping"
     ]
 }
 
